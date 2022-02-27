5bb4f3bcb6051780a7e633e743b560a7e5406d26
Chase Rehm <chsrhm@gmail.com>
2022-02-26 22:48:36 -0500

    implements the Guess game and Die class of Program 2 assignment

--- Rehm Dice Game 2800C/Die.java
+++ Rehm Dice Game 2800C/Die.java
@@ -1,2 +1,36 @@
+import java.util.Random;
+
 public class Die {
+    Random randGen = new Random();
+
+    private int dieNum;
+
+    /**
+     * Default constructor. Creates a Die object and
+     * sets this object's dieNum field to a random
+     * number between 1 and 6 inclusive.
+     */
+    public Die () {
+        // default constructor
+        this.dieNum = randGen.nextInt(6) + 1;
+    }
+
+    /**
+     * Generates a random number between 1 and 6 inclusive.
+     * Returns that random number and sets this objects dieNum
+     * field with that number as well.*/
+    public int rollDie() {
+        this.dieNum = randGen.nextInt(6) + 1;
+        return this.dieNum;
+    }
+
+    /**
+     * Returns this Die object's dieNum field.
+     *
+     */
+    public int getDieNum() {
+        return this.dieNum;
+    }
+
+
 }
--- Rehm Dice Game 2800C/Guess.java
+++ Rehm Dice Game 2800C/Guess.java
@@ -1,2 +1,72 @@
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.Scanner;
+
 public class Guess {
+    public static void main(String[] args) {
+        // variables used in the game. numOfRolls is initialized with 1 because the user starts with their first roll
+        int userGuess;
+        int diceSum;
+        int numOfRolls = 1;
+
+        // creating two independent dice objects
+        Die die1 = new Die();
+        Die die2 = new Die();
+
+        // new Scanner object for accepting user input
+        Scanner keyboard = new Scanner(System.in);
+
+        // bannerChar for welcome message and continueKey for main while loop sentinel value
+        String bannerChar = "*";
+        String continueKey = "y";
+
+        // welcome banner message
+        for (int i = 0; i < 6; ++i) {
+            if (i != 2) {
+                System.out.println(bannerChar.repeat(50));
+            } else {
+                System.out.print(bannerChar.repeat(11));
+                System.out.print(" Welcome to the 3 Roll Bet! ");
+                System.out.println(bannerChar.repeat(11));
+            }
+        }
+        System.out.println();
+
+        // game rules explanation
+        System.out.println("First, choose a number between 2 and 12. Then you will roll a pair of dice 3 times.");
+        System.out.println("If the sum of the dice equals your number within the 3 rolls, you win! Good luck!");
+        System.out.println();
+
+        // user chooses their number
+        System.out.print("You must choose a number between 2 and 12: ");
+        userGuess = keyboard.nextInt();
+
+        // while loop that forces a user to choose an int between 2 and 12 inclusive
+        while (userGuess < 2 || userGuess > 12) {
+            System.out.print("That's not a valid number! Please try again: ");
+            userGuess = keyboard.nextInt();
+        }
+
+        // main while loop of the game. the loop will exit if the user hits their number, if the numOfRolls hits 3, or if the user quits (boooooo)
+        while ( !continueKey.equals("n") ) {
+            diceSum = die1.rollDie() + die2.rollDie();
+            System.out.println("Roll " + numOfRolls + " was a " + diceSum + ".");
+            if (diceSum == userGuess) {
+                System.out.print("You rolled your number! Congratulations, you win the game!");
+                break;
+            } else if (numOfRolls == 3){
+                System.out.print("You didn't roll your number within 3 rolls. You lose, house wins!");
+                break;
+            } else {
+                System.out.print("That's not your number, try again? Enter 'n' to quit, any other letter to continue: ");
+                ++numOfRolls;
+                continueKey = keyboard.next();
+            }
+        }
+
+        // added incentive to not quit next time
+        if (continueKey.equals("n")) {
+            System.out.println("Nobody likes a quitter.");
+        }
+    }
 }
